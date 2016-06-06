17893af3a01dc56ce684c0ca749cf7df48209336
Gregory Ristow <gristow@gmail.com>
2016-06-06 13:34:59 -0400

    feat(stems): add groups for stems

--- src/stem.js
+++ src/stem.js
@@ -145,6 +145,7 @@ Vex.Flow.Stem = (function() {
 
       // Draw the stem
       ctx.save();
+      ctx.openGroup("stem");
       this.applyStyle(ctx);
       ctx.beginPath();
       ctx.setLineWidth(Stem.WIDTH);
@@ -152,6 +153,7 @@ Vex.Flow.Stem = (function() {
       ctx.lineTo(stem_x, stem_y - this.getHeight());
       ctx.stroke();
       ctx.restore();
+      ctx.closeGroup();
     }
   };
 
